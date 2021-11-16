c0e98e9b8db38be1bc587c6f3c3b4eafe5dd8844
mfmjr <88806962+mfmjr@users.noreply.github.com>
2021-11-15 22:15:23 -0800

    Programação Avançada - Lista 1

---
+++ questao11/questao11.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main (){
+
+float aloha[10], coisas[10][5], *pf, value = 2.2;
+int i=3;
+
+//aloha[2] = value;               //Válido
+//scanf("%f", &aloha);            //Válido       
+//aloha = value";                 //Inválido
+//printf("%f", aloha);            //Válido
+//coisas[4][4] = aloha[3];        //Válido
+//coisas[5] = aloha;              //Inválido
+//pf = value;                     //Válido
+//pf = aloha;                     //Válido
+
+printf("%f\n", aloha);
+
+    return 0;
+}
