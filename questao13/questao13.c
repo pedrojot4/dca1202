3a84c166503d31cb2f92f207b3078e2fd84f7958
mfmjr <88806962+mfmjr@users.noreply.github.com>
2021-11-19 00:03:07 -0800

    Programação Avançada - Lista 1

--- questao13/questao13.c
+++ questao13/questao13.c
@@ -1,18 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main (){
+void funcao_ordenacao(int n, int *a) /* Função de ordem crescente. */
+{
 
-int tam_vector, i, k;
-float *var, aux;
+    int aux;
+    for (int i = 0; i < n; i++)         // #===========================================================#
+    {                                   // #  Este for fez-se necessário a sua criação, para realizar  #
+        for (int k = i + 1; k < n; k++) // #  a comparação entre o vetor anterior (var[i]) com o vetor #
+                                        // # de posição a frente (var[k]) do mesmo. Perceba que este   #
+        {                               // # vetor começa na posição i+1.                              #
+                                        // #===========================================================#
 
-printf("Digite o valor do tamanho do vetor: ");
-scanf("%d", &tam_vector);
- 
-var = malloc(tam_vector *sizeof(int)); // Tamanho do vetor tipo int, ou seja, tam_vector* 4 bytes = (var) em bytes.
+            if (a[i] > a[k])
+            {                // #===============================================================================================#
+                aux = a[i];  // # Suponha que no a[i=0] = 5 e o a[k=1]= 2. Logo, o contéudo de a[i=0] irá para a variável aux.  #
+                a[i] = a[k]; // # O conteúdo que estava em a[k=1]=2 irá para a[i=0], atualizando assim, o seu conteúdo.         #
+                a[k] = aux;  // # Dessa forma, o contéudo do aux=5 irá para o a[k=1]. Trocando assim suas posições.             #
+            }                // #===============================================================================================#
+        }
+    }
+}
 
-/*
+int main()
+{
+
+    int tam_vector, i, k;
+    float *var, aux;
+
+    printf("Digite o valor do tamanho do vetor: ");
+    scanf("%d", &tam_vector);
+
+    var = malloc(tam_vector * sizeof(int)); // Tamanho do vetor tipo int, ou seja, tam_vector* 4 bytes = (var) em bytes.
+
+    /* 
 #=================#
 # char:   1 byte  #
 # int:    4 bytes #
@@ -21,72 +43,32 @@ var = malloc(tam_vector *sizeof(int)); // Tamanho do vetor tipo int, ou seja, ta
 #=================#
 */
 
-if (var == NULL)
-{
+    if (var == NULL)
+    {
 
-printf ("Nao ha memoria dinamica suficiente para a operacao! \n");
+        printf("Nao ha memoria dinamica suficiente para a operacao! \n");
+    }
 
-}
-
-   else
-   {   
-       
-       for (i = 0; i < tam_vector ; i++)
-       {        
-
-           printf("Digite o numero do vetor[%d]: ", i);  //Digitar o tamanho do vetor.
-           scanf("%f", &var[i]);  
-       }
+    else
+    {
 
         for (i = 0; i < tam_vector; i++)
-        {         
-        
-            for (k = i+1; k < tam_vector ; k++)         
-            {
-                    /* 
-                    #===========================================================#
-                    #  Este for fez-se necessário a sua criação, para realizar  #
-                    #  a comparação entre o vetor anterior (var[i]) com o vetor #
-                    # de posição a frente (var[k]) do mesmo. Perceba que este   #
-                    # vetor começa na posição i+1.                              #
-                    #===========================================================#
-                    */           
-                       
+        {
 
-                    if (var[i] > var[k])        
-                    {       
-                        aux = var[i];
-                        var[i] = var[k];
-                        var[k] = aux;
-    
-                    } 
-                            /* 
-                            #===========================================================#
-                            # No if acontece a alocação de memória do vetor. Tem-se uma #
-                            # variável do tipo auxilar (aux). Nela, será armazenado o   #
-                            # conteúdo do var[i] atual, pois este é maior que var[k].   #
-                            # Logo, o valor de var[k] irá para var[i] e este será       #
-                            # atualizado. O conteúdo que estava no auxiliar, antes da   #
-                            # atualização irá para var[k].                              #
-                            #===========================================================#
-                            */
-    
-                                   
-            }
+            printf("Digite o numero do vetor[%d]: ", i); //Digitar o tamanho do vetor.
+            scanf("%f", &var[i]);
         }
-              
 
-                            for (i = 0; i < tam_vector; i++)
-                            {
+        funcao_ordenacao(tam_vector, var);
 
-                             /* Impressão dos valores em ordem crescente. */
-                               printf ("O vetor[%d]: %f\n", i, var[i]);                                
+        for (i = 0; i < tam_vector; i++)
+        {
 
-                            }     
+            /* Impressão dos valores em ordem crescente. */
+            printf("O vetor[%d]: %f\n", i, var[i]);
+        }
+    }
 
-   }    
-
-   
     free(var);
     return 0;
 }
