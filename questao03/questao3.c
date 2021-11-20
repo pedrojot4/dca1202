ba7cd430148dd662494ec44ec58ea7710a1847c7
mfmjr <88806962+mfmjr@users.noreply.github.com>
2021-11-19 17:59:38 -0800

    Update questao3.c

--- questao03/questao3.c
+++ questao03/questao3.c
@@ -6,13 +6,13 @@ int main (){
 
  int i, *p=2, *q=2;
 
-    // p = i;                      Seria mais adequado criar uma variável ao invés de um poteiro.
+    // p = i;                      Não é adequado. Seria mais adequado criar uma variável ao invés de um poteiro.
     // q = &j;                     Correto
     // p = &*&i;                   Correto
     //i = (*&)j;                   Não é possível acessar o endereço e o conteúdo ao mesmo tempo
     // i = *&j;                    Correto
-    // i = *&*&j;                  Ela fica repetindo a mesma operação por 2x.
-    //q = *p;                      ????
+    // i = *&*&j;                  Não é adequado. Ela fica repetindo a mesma operação por 2x.
+    //q = *p;                      Correto.
     // i = (*p)++ + *q;            Incorreto
 
 printf("i = %d", i);
